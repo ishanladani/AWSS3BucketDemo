a2d7a076b7854978a252970800c000121188299c
Ishan Ladani <ishanladani16@gmail.com>
2021-11-19 12:05:24 +0530

    AWS S3 bucket demo

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Home from './src/Home';
+
+const App = () => (
+  <View style={{flex: 1}}>
+    <Home />
+  </View>
+);
+
+export default App;
